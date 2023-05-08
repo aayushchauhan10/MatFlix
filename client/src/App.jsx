b5eadc5bbc6499364010039a973a4c2926345bc2
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-08 17:45:22 +0530

    Front end Setup Done

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="font-bold underline text-center">
+        Hello, This is the NetFlix Clone
+      </p>
+    </>
+  );
+}
+
+export default App;
