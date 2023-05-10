5b8d8a9c4e95bbf43f234b9ee7ebd891fc4da67a
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-10 23:48:34 +0530

    Update Questions.jsx

---
+++ client/src/components/layout/Questions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { questions } from "../../constants/index";
+
+const Question = ({ title, description }) => {
+  <div className="flex w-3/4 h-[30px] hover:[#223263] active:[#223263] bg-[#122044]">
+    <div className=""></div>
+  </div>;
+};
+
+const Questions = () => {
+  return <div className="text-white bg-[#01081C]">Questions</div>;
+};
+
+export default Questions;
