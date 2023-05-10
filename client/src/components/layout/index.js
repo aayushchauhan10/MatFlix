1f76669e7a2a0311523e93a3cb927cfa14360c02
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-10 23:48:19 +0530

    Added Questions

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header.jsx";
 export { default as Home } from "./Home.jsx";
 export { default as Footer } from "./Footer.jsx";
 export { default as ServiceCard } from "./ServiceCard.jsx";
+export { default as Questions } from "./Questions.jsx";
