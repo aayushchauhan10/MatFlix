6f7b945288e6b2fdc87b6af2084a720a900fc7f2
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-11 19:47:13 +0530

    Created FAQ section

--- client/src/components/layout/Questions.jsx
+++ client/src/components/layout/Questions.jsx
@@ -1,14 +1,74 @@
-import React from "react";
-import { questions } from "../../constants/index";
-
-const Question = ({ title, description }) => {
-  <div className="flex w-3/4 h-[30px] hover:[#223263] active:[#223263] bg-[#122044]">
-    <div className=""></div>
-  </div>;
-};
+import React, { useState } from "react";
+import { questions } from "../../constants"; // Assuming the questions array is exported from 'data.js'
 
 const Questions = () => {
-  return <div className="text-white bg-[#01081C]">Questions</div>;
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <div className="py-6 text-white bg-[#01081C]">
+      <h1 className="font-bold text-[35px] px-16 pt-16 pb-4">
+        Frequently Asked Questions
+      </h1>
+      {questions.map((question, index) => (
+        <div
+          key={index}
+          onClick={() => handleClick(index)}
+          className="mb-4 mx-24 bg-[#122044] font-netflix p-5"
+        >
+          <div className="flex justify-between hover:bg[#223263] focus:bg-[#223263] active:bg[#223263] items-center cursor-pointer">
+            <h2 className="text-[25px]">{question.title}</h2>
+            <span>
+              {activeIndex === index ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </span>
+          </div>
+          <div
+            className={` text-white  ${
+              activeIndex === index
+                ? "opacity-100 max-h-100 text-[20px] mt-2"
+                : "opacity-0 max-h-0"
+            } transition-all duration-300`}
+          >
+            <hr className="pb-2" />
+            {question.description}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Questions;
