f2392a66be96df6a35f03ec6d0d9cef0eef92fe9
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-11 19:47:29 +0530

    Fixed some typos and added mroe questions

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -32,36 +32,42 @@ const services = [
 
 const questions = [
   {
+    index: 0,
     title: "What is Netflix?",
-    desciption:
+    description:
       "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, animes, documentaries and more - on thousands of interconnected devices. You can watch as much as you want, whenever you want, without a single ad, all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week.",
   },
   {
-    title: "How much does Netflix costr?",
+    index: 1,
+    title: "How much does Netflix cost?",
     description:
       "Watch Netflix on your smartphone, tablet, Smart TV, laptop or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts. ",
   },
   {
+    index: 2,
     title: "Where can I watch?",
-    desciption:
+    description:
       "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
   },
   {
+    index: 3,
     title: "How do I cancel?",
     description:
       "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account in easily two clicks. There are no cancellation fees - start or stop your account anytime.",
   },
   {
+    index: 4,
     title: "What can I watch on Netflix?",
-    desciption:
+    description:
       "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals and more. Watch as much as you want, anytime you want.",
   },
 
   {
+    index: 5,
     title: "Is Netflix good for kids?",
-    desciption:
+    description:
       "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
   },
 ];
 
-export { services };
+export { services, questions };
